c490177acff28c87bbc833c665d054c84707944a
Jesus Anaya <jesus.anaya.dev@gmail.com>
2014-07-30 02:25:40 -0700

    Remove unnecessary content in the app

--- mezzanine_api/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from mezzanine.blog.models import BlogPost
-
-
-class DisplayableSerializer(serializers.HyperlinkedModelSerializer):
-    site = serializers.RelatedField(source='site', required=False)
